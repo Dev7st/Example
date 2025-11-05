c9d0092bee8077fd1fbcbb7eed24ca52968ce4f1
bjh <imganchi@gmail.com>
2025-11-05 17:59:40 +0900

    react: component styling

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,24 @@
-import Example from './component/Example';
+import styled from "styled-components";
+
+const Box = styled.div`
+	width:100px;
+	height:100px;
+	background-color:green;
+	border-radius:8px;
+	
+	p{
+	color:red;
+	}
+`
 
 function App() {
 
-    return (
-		<div>
-        	<Example />
-    	</div>
+	return(
+		<>
+			<Box>
+				<p>asdf</p>
+			</Box>
+		</>
 	);
 }
 
