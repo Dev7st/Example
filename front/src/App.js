e9c2214c252e932e0f4402d150d71bc7a59980c9
bjh <imganchi@gmail.com>
2025-11-05 17:41:14 +0900

    react: filter--

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,33 @@
 import { useState } from 'react';
 
 function App() {
-	const [people,setPeople] = useState(["마리오","짱구","철수","루이지","유리"]);
+	const [people,setPeople] = useState([
+		{
+			id:0,
+			name: "크레올라",
+			profession: "의사"
+		},
+		{
+			id:1,
+			name: "마리오",
+			profession: "개발자"
+		},
+		{
+			id:2,
+			name: "루이지",
+			profession: "개발자"
+		}
+	]);
 
-	const listItem = people.map((person,index) => {
-		return (
-			<li key={index}>{person}</li>
-		)
-	});
+	const data = people.filter((person) => {
+		return person.profession === "개발자"
+	})
+
+	console.log(data);
 
   return (
 	<div>
-		{listItem}
+
 	</div>
   );
 }
