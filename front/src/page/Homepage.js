50d5ffae11e3a992fb15891b9c9c4718bbfad20c
bjh <imganchi@gmail.com>
2025-11-05 18:17:01 +0900

    react: path variable

--- front/src/page/Homepage.js
+++ front/src/page/Homepage.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Homepage = () => {
-	const navigate = useNavigate();
-
-	const handleClick = () => {
-		alert("로그인 페이지로 이동합니다.");
-		navigate("/login");
-	}
+	let {id} = useParams();
+	console.log(id);
 
 	return (
 		<div>
-			<button onClick={handleClick}>Go Login</button>
+			
 		</div>
 	);
 }
 
 export default Homepage;
+
+//링크에 /id값을 넣어주면 콘솔에 출력됨
