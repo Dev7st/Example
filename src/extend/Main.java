435017d92eb4ab9cc4b3632cab61b00b80a508cc
bjh <imganchi@gmail.com>
2026-01-31 21:08:15 +0900

    java: 오버라이드의 사용 여부는 인스턴스화한 클래스의 메서드를 기준으로 사용함

--- src/extend/Main.java
+++ src/extend/Main.java
@@ -2,8 +2,8 @@ package extend;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat();
+        Animal animal = new Cat();
 
-        cat.eat();
+        animal.eat();
     }
 }
