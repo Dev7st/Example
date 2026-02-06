6800ff49ff72e5203cab3dc6b1db5254febaebb3
bjh <imganchi@gmail.com>
2026-02-06 13:20:41 +0900

    java: 실습_가위바위보

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,43 @@
-import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
+        String[] hand = {"가위","바위","보"};
 
-        try(FileInputStream fileInputStream = new FileInputStream("index.txt");
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-            FileOutputStream fileOutputStream = new FileOutputStream("output.txt");
-            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream)){
-            int i;
-            while ((i= bufferedInputStream.read()) !=-1){
-                bufferedOutputStream.write(i);
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            String computerPick = hand[random.nextInt(3)];
+            try {
+                String myPick = hand[sc.nextInt()];
+                if(myPick == computerPick){
+                    System.out.println("나 : "+myPick);
+                    System.out.println("컴퓨터 : "+computerPick);
+                    System.out.println("비겼습니다.");
+                } else if(myPick == "가위" && computerPick == "바위"){
+                    System.out.println("나 : "+myPick);
+                    System.out.println("컴퓨터 : "+computerPick);
+                    System.out.println("졌습니다.");
+                } else if (myPick == "바위" && computerPick == "보") {
+                    System.out.println("나 : "+myPick);
+                    System.out.println("컴퓨터 : "+computerPick);
+                    System.out.println("졌습니다.");
+                } else if (myPick == "보" && computerPick == "가위") {
+                    System.out.println("나 : "+myPick);
+                    System.out.println("컴퓨터 : "+computerPick);
+                    System.out.println("졌습니다.");
+                } else {
+                    System.out.println("나 : "+myPick);
+                    System.out.println("컴퓨터 : "+computerPick);
+                    System.out.println("이겼습니다.");
+                }
+            } catch (Exception e){
+                System.out.println("다시 입력하세요");
+                sc.nextLine();
+                continue;
             }
-        } catch (IOException e){
-
         }
-
-        long endTime = System.currentTimeMillis();
-        System.out.println(endTime - startTime);
     }
 }
